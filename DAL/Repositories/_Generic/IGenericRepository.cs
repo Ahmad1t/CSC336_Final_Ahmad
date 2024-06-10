1dc451d2a76dda58528e28435a9cc907f4f48470
Ahmad1t <ahmad_tohmeh@outlook.com>
2024-06-10 09:59:13 +0300

    Add (Repository, Serive,Controller) Generic and Mapping and Wrapping

---
+++ DAL/Repositories/_Generic/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories._Generic
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T GetById(int id);
+        T Add(T obj);
+        T Update(T obj);
+        bool Delete(int id);
+        bool Delete(T obj);
+    }
+
+}
